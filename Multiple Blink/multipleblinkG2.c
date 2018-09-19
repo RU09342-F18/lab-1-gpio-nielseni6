5fd97cb82006a78bbb506f2cf42aa63dd6d96f79
nielseni6 <36169018+nielseni6@users.noreply.github.com>
2018-09-19 08:21:16 -0400

    Update multipleblinkG2.c

--- Multiple Blink/multipleblinkG2.c
+++ Multiple Blink/multipleblinkG2.c
@@ -9,14 +9,13 @@ void multipleblinkG2(void)
     WDTCTL = WDTPW | WDTHOLD;               // stop watchdog timer
     P1DIR |= 0x01;                          // configure P1.0 as output
     P1DIR |= BIT6;                          // configure P1.6 as output
-    P1DIR &= ~0x08;                         // configure P1.3 as input
-
-
+    
     volatile unsigned int i;                // volatile to prevent optimization
     int g = 0;                              // counter to set green led blink interval - waits to reach x
     int r = 0;                              // counter to set red led blink interval - waits to reach x/2
     int x = 32;                             // blink interval - the higher the slower the blink
-    while(1)
+    
+    while(1)                                // infinite loop
     {
         if (r >= (x/2)){                    // waits for g to reach x/2 (blink delay)
             P1OUT ^= BIT6;                  // toggle P1.6(LED)
