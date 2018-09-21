fc9c438e3959e2dcea868e4115fd55f76baa790b
nielseni6 <36169018+nielseni6@users.noreply.github.com>
2018-09-21 12:42:13 -0400

    Update and rename buttonblinkG2.c to main.c

--- Button Blink/MSP430G2553/main.c
+++ Button Blink/MSP430G2553/main.c
@@ -1,10 +1,6 @@
 #include <msp430.h>				
 
-
-/**
- * buttonblinkG2.c
- */
-void buttonblinkG2(void)
+void main(void)
 {
 	WDTCTL = WDTPW | WDTHOLD;	        // stop watchdog timer
 	P1DIR |= 0x01;			  	// configure P1.0 as output
