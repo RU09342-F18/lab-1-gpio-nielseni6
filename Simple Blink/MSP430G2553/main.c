8fe221d193bd37a26da1398c466ab146c509c3f2
nielseni6 <36169018+nielseni6@users.noreply.github.com>
2018-09-21 12:45:10 -0400

    Update and rename simpleblinkG2.c to main.c

--- Simple Blink/MSP430G2553/main.c
+++ Simple Blink/MSP430G2553/main.c
@@ -1,10 +1,6 @@
 #include <msp430.h>
 
-
-/**
- * simpleblinkG2.c
- */
-void simpleblinkG2(void)
+void main(void)
 {
     WDTCTL = WDTPW | WDTHOLD;               // stop watchdog timer
     P1DIR |= 0x01;                          // configure P1.0 as output
